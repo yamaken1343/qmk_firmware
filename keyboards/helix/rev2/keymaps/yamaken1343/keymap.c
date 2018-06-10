e05495db038057e05e2b7036c42e3c62299a780c
yamaken <yamaken1343@gmail.com>
2018-06-10 14:36:50 +0900

    add cat copy paste undo key

--- keyboards/helix/rev2/keymaps/yamaken1343/keymap.c
+++ keyboards/helix/rev2/keymaps/yamaken1343/keymap.c
@@ -59,7 +59,11 @@ enum custom_keycodes {
   EISU,
   KANA,
   RGBRST,
-  TENKEY
+  TENKEY,
+    CUT,
+    COPY,
+    PSTE,
+    UNDO
 };
 
 enum macro_keycodes {
@@ -128,18 +132,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | F12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             | Bksp | Left |  Up  | Down |Right |  -   |
+   * |      |BrowBk|      |      |BrowFw|      |             | Bksp | Left |  Up  | Down |Right |  -   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  | Del  | Home |PageUp|PageDn| End  |      |
+   * |      | Undo | Cut  | Copy |Paste |      |  (   |   )  | Del  | Home |PageUp|PageDn| End  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |ADJUST| Back | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = KEYMAP( \
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-      KC_GRV,  KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_BSPC, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_MINS, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_DEL,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, \
+      KC_GRV,  KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12, \
+      _______, KC_WBAK, _______, _______, KC_WFWD, _______,                   KC_BSPC, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_MINS, \
+      _______, UNDO   , CUT    , COPY   , PSTE   , _______, KC_LPRN, KC_RPRN, KC_DEL,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, ADJUST, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
@@ -149,9 +153,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  F12 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |   _  |
+   * |      |BrowBk|      |      |BrowFw|      |             |      |   -  |   =  |   [  |   ]  |   _  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |   _  |   +  |   {  |   }  |      |
+   * |      | Undo | Cut  | Copy |Paste |      |      |      |      |   _  |   +  |   {  |   }  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |ADJUST|      |      |      | Back | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
@@ -159,20 +163,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = KEYMAP( \
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_UNDS, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
-      _______, _______, _______, _______, _______, ADJUST, _______, _______, _______, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, KC_WBAK, _______, _______, KC_WFWD, _______,                   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_UNDS, \
+      _______, UNDO   , CUT    , COPY   , PSTE   , _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+      _______, _______, _______, _______, _______, ADJUST , _______, _______, _______, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      | Reset|RGBRST|      |      |      |             |      |      |      |      |      |  F12 |
+   * |Reset |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | CAPS |Insert|      |Aud on|Audoff| Mac  |             | Win  |Qwerty|      |Dvorak|      |      |
+   * | CAPS |Insert|      |Aud on|Audoff| Mac  |             | Win  |Qwerty|Tenkey|Dvorak|      |  F12 |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |PRTSCR|      |      |      |      |return|      |      | swap |      |RGB ON| HUE+ | SAT+ | VAL+ |
+   * |PRTSCR|      |      |      |      |return|      |      | swap |RGBRST|RGB ON| HUE+ | SAT+ | VAL+ |
    * |      |      |      |      |      |caps-ctrl    |      |caps-ctrl    |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      | MODE | HUE- | SAT- | VAL- |
@@ -180,13 +184,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] =  KEYMAP( \
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_F12, \
-      KC_CAPS, KC_INS , _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  _______, DVORAK,  _______, _______, \
-      KC_PSCR, _______, _______, _______, _______, CC_NORM, _______, _______, CC_SWAP, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+      RESET  , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+      KC_CAPS, KC_INS , _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  TENKEY , DVORAK,  _______, KC_F12, \
+      KC_PSCR, _______, _______, _______, _______, CC_NORM, _______, _______, CC_SWAP, RGBRST , RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
       ),
         
-   /* Ten key pad  0.666
+   /* Ten key pad
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |  ESC |      |      |      |      |      |             |NUMLK | Tab  |   /  |   *  |   =  | Bksp |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -194,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl | Left | Down | Right|      |      |             |      |   4  |   5  |   6  |   +  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Shift |      |      |      |      |      |      |      |      |   1  |   2  |   3  |   ,  |Shift |
+   * |Shift | Undo | Cut  | Copy |Paste |      |      |      |      |   1  |   2  |   3  |   ,  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |  SP  |Enter |   0  |   .  | Enter|DVORAK|ADJUST|
    * `-------------------------------------------------------------------------------------------------'
@@ -203,7 +207,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    KC_NLCK, KC_TAB,  KC_PSLS, KC_PAST, KC_PEQL, KC_BSPC, \
       _______, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_DEL, \
       KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_ENT, \
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PCMM, KC_RSFT, \
+      KC_LSFT, KC_UNDO, KC_CUT , KC_COPY, KC_PSTE, XXXXXXX, _______, _______, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PCMM, KC_RSFT, \
       _______, _______, _______, _______, _______, _______, _______, KC_SPC,  KC_ENT,  KC_P0,   KC_PDOT, KC_PENT, DVORAK,  ADJUST \
       )
 };
@@ -369,8 +373,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         persistent_default_layer_set(1UL << _TENKEY);
         default_layer_current_mode = _TENKEY;
         return false;
-        break;
       }
+      break;
+    case UNDO:
+      if(record->event.pressed){
+        if(keymap_config.swap_lalt_lgui == false){
+          SEND_STRING(SS_LGUI("z"));
+        }else{
+          SEND_STRING(SS_LCTRL("z"));
+        }
+      }
+      return false;
+      break;
+    case COPY:
+      if(record->event.pressed){
+        if(keymap_config.swap_lalt_lgui == false){
+          SEND_STRING(SS_LGUI("c"));
+        }else{
+          SEND_STRING(SS_LCTRL("c"));
+        }
+      }
+      return false;
+      break;
+    case PSTE:
+      if(record->event.pressed){
+        if(keymap_config.swap_lalt_lgui == false){
+          SEND_STRING(SS_LGUI("v"));
+        }else{
+          SEND_STRING(SS_LCTRL("v"));
+        }
+      }
+      return false;
+      break;
+    case CUT:
+      if(record->event.pressed){
+        if(keymap_config.swap_lalt_lgui == false){
+          SEND_STRING(SS_LGUI("x"));
+        }else{
+          SEND_STRING(SS_LCTRL("x"));
+        }
+      }
+      return false;
+      break;
   }
   return true;
 }
