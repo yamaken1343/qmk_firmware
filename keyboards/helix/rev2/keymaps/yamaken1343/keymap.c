28fb03307ef62408c06897f888d27074f67d6c95
yamaken <yamaken1343@gmail.com>
2018-08-12 15:16:27 +0900

    little fix

--- keyboards/helix/rev2/keymaps/yamaken1343/keymap.c
+++ keyboards/helix/rev2/keymaps/yamaken1343/keymap.c
@@ -206,7 +206,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PEQL, KC_DEL, \
       KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,                   _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_ENT, \
       KC_LSFT, KC_UNDO, KC_CUT , KC_COPY, KC_PSTE, XXXXXXX, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, KC_RSFT, \
-      _______, _______, _______, _______, _______, _______, _______, KC_SPC,  KC_ENT,  KC_P0,   KC_PDOT, KC_PCMM, DVORAK,  ADJUST \
+      _______, _______, _______, _______, _______, _______, _______, KC_SPC , _______, KC_P0,   KC_PDOT, KC_PCMM, DVORAK,  ADJUST \
       )
 };
 
